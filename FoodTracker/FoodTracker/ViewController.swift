becb01535b3074176f6e7288202e66b238bad898
yuyadomae <y.domae@leihauoli.com>
2018-12-28 14:34:48 +0900

    プロジェクト作成

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Yuya Domae on 2018/12/28.
+//  Copyright © 2018年 Yuya Domae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
