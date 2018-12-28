9cadab64b2ff3a64232c830af82fc75545e82e82
yuyadomae <y.domae@leihauoli.com>
2018-12-28 16:50:56 +0900

    アクション追加

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var meanNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    //MARK: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        print("ボタンがクリックされました");
+        meanNameLabel.text = "default text";
+    }
 }
 
